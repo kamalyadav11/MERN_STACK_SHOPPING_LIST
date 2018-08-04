ea4f0a81973effdb176a6c6f8df2bb8194a55dd6
kamal11 <kyzx11@gmail.com>
2018-08-04 18:54:54 +0530

    Prepared Package.json and server.js for heroku deployment

--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
+const path = require("path");
 
 const port = process.env.PORT || 5000;
 
@@ -25,6 +26,16 @@ mongoose.Promise = global.Promise;
 
 //Use Routes
 app.use("/api/items", items); //first argument is the path and second is the file name defined above
+
+//Serve static assests if in production
+if (process.env.NODE_ENV === "production") {
+  //Set static folder
+  app.use(express.static("client/build"));
+
+  app.get("*", (req, res) => {
+    res.sendFile(path.resolve(__dirname, "client", "build", "index.html"));
+  });
+}
 
 app.listen(port, () => {
   console.log(`Server is listening on port ${port}`);
